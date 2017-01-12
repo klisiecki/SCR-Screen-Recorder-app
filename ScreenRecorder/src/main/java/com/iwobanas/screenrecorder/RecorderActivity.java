8c0edd4a8a98de4a58c9ae17c659e45be381a394
Karol Lisiecki <lisiecki.karol@gmail.com>
2017-01-12 15:04:13 +0100

    handling overlay permission on API >= 23

--- ScreenRecorder/src/main/java/com/iwobanas/screenrecorder/RecorderActivity.java
+++ ScreenRecorder/src/main/java/com/iwobanas/screenrecorder/RecorderActivity.java
@@ -2,11 +2,15 @@ package com.iwobanas.screenrecorder;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
-
 import com.iwobanas.screenrecorder.settings.Settings;
 
 public class RecorderActivity extends Activity {
+
+    public final static int REQUEST_CODE = -1010101;
+
     /**
      * Called when the activity is first created.
      */
@@ -17,10 +21,29 @@ public class RecorderActivity extends Activity {
         if (savedInstanceState == null && GusherDialogFragment.shouldShow(this)) {
             new GusherDialogFragment().show(getFragmentManager(), GusherDialogFragment.FRAGMENT_TAG);
         } else {
-            Intent intent = new Intent(this, RecorderService.class);
-            intent.setAction(RecorderService.LOUNCHER_ACTION);
-            startService(intent);
-            finish();
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && !android.provider.Settings.canDrawOverlays(this)) {
+                Intent intent = new Intent(android.provider.Settings.ACTION_MANAGE_OVERLAY_PERMISSION,
+                        Uri.parse("package:" + getPackageName()));
+                startActivityForResult(intent, REQUEST_CODE);
+            } else {
+                startRecorderService();
+            }
         }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_CODE) {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && android.provider.Settings.canDrawOverlays(this)) {
+                startRecorderService();
+            }
+        }
+    }
+
+    private void startRecorderService() {
+        Intent intent = new Intent(this, RecorderService.class);
+        intent.setAction(RecorderService.LOUNCHER_ACTION);
+        startService(intent);
+        finish();
     }
 }
