fee2a6e545a224003ebff38e5900b2c917fc2f11
klisiecki <lisiecki.karol@gmail.com>
2017-01-19 12:40:10 +0100

    add requests for dangerous permissions in Android >= 6.0

--- ScreenRecorder/src/main/java/com/iwobanas/screenrecorder/RecorderActivity.java
+++ ScreenRecorder/src/main/java/com/iwobanas/screenrecorder/RecorderActivity.java
@@ -1,15 +1,27 @@
 package com.iwobanas.screenrecorder;
 
+import android.Manifest;
 import android.app.Activity;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+
 import com.iwobanas.screenrecorder.settings.Settings;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class RecorderActivity extends Activity {
 
     public final static int REQUEST_CODE = -1010101;
+    private final static String[] PERMISSIONS = {Manifest.permission.WRITE_EXTERNAL_STORAGE,
+            Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.ACCESS_FINE_LOCATION,
+            Manifest.permission.RECORD_AUDIO};
 
     /**
      * Called when the activity is first created.
@@ -26,7 +38,7 @@ public class RecorderActivity extends Activity {
                         Uri.parse("package:" + getPackageName()));
                 startActivityForResult(intent, REQUEST_CODE);
             } else {
-                startRecorderService();
+                checkDangerousPermissions();
             }
         }
     }
@@ -35,9 +47,29 @@ public class RecorderActivity extends Activity {
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         if (requestCode == REQUEST_CODE) {
             if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && android.provider.Settings.canDrawOverlays(this)) {
-                startRecorderService();
+                checkDangerousPermissions();
             }
         }
+    }
+
+    private void checkDangerousPermissions() {
+        List<String> toRequestPermissions = new ArrayList<>();
+        for (String permission : PERMISSIONS) {
+            if (ContextCompat.checkSelfPermission(this, permission) != PackageManager.PERMISSION_GRANTED) {
+                toRequestPermissions.add(permission);
+            }
+        }
+        if (toRequestPermissions.isEmpty()) {
+            startRecorderService();
+        } else {
+            ActivityCompat.requestPermissions(this, toRequestPermissions.toArray(new String[toRequestPermissions.size()]), 0);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        startRecorderService();
     }
 
     private void startRecorderService() {
