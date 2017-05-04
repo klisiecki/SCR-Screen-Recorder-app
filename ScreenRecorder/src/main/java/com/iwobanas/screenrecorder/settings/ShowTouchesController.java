3656e2a597ec0068161bf86c2e49674e85858f77
klisiecki <lisiecki.karol@gmail.com>
2017-05-04 15:17:44 +0200

    attempt to fix Show Touches option

--- ScreenRecorder/src/main/java/com/iwobanas/screenrecorder/settings/ShowTouchesController.java
+++ ScreenRecorder/src/main/java/com/iwobanas/screenrecorder/settings/ShowTouchesController.java
@@ -2,14 +2,20 @@ package com.iwobanas.screenrecorder.settings;
 
 import android.content.ContentResolver;
 import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Build;
+import android.provider.*;
 import android.provider.Settings.System;
 
 public class ShowTouchesController {
     private final String SHOW_TOUCHES_SETTING = "show_touches";
 
     private ContentResolver contentResolver;
+    private final Context context;
 
     public ShowTouchesController(Context context) {
+        this.context = context;
         contentResolver = context.getContentResolver();
     }
 
@@ -20,7 +26,13 @@ public class ShowTouchesController {
 
     public void setShowTouches(boolean show) {
         if (getShowTouches() != show) {
-            System.putInt(contentResolver, SHOW_TOUCHES_SETTING, show ? 1 : 0);
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && !android.provider.Settings.System.canWrite(context)) {
+                Intent intent = new Intent(android.provider.Settings.ACTION_MANAGE_WRITE_SETTINGS,
+                        Uri.parse("package:" + context.getPackageName()));
+                context.startActivity(intent);
+            } else {
+                System.putInt(contentResolver, SHOW_TOUCHES_SETTING, show ? 1 : 0);
+            }
         }
     }
 }
